65d40ca6fb33bb0dbb84324f526191a1f3499c55
sukyeong <sasa02336@gmail.com>
2025-08-19 17:20:53 +0900

    add module,controller,provider,middleware

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Delete, Get, Param, Post, Put, Query, Req } from '@nestjs/common';
+import { CreateCatDto } from './dto/CreateCatDto';
+import { CatsService } from './cats.service';
+import { Cat } from './interface/Cat.interface';
+
+@Controller('cats')
+export class CatsController {
+    constructor(private catsService: CatsService) {}
+
+    @Post()
+    create(@Body() createCatDto: CreateCatDto) {
+        return this.catsService.create(createCatDto);
+    }
+
+    @Get()
+    async findAll(): Promise<Cat[]> {
+        return this.catsService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return `This action returns a #${id} cat`;
+    }
+
+    // @Put(':id')
+    // update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+    //     return `This action updates a #${id} cat`;
+    // }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return `This action removes a #${id} cat`;
+    }
+}
